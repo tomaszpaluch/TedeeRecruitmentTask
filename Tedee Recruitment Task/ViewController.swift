8871e94d69495b11c74699ed341dc39c06b69948
Tomasz Paluch <paluch.t@wp.pl>
2022-06-30 12:03:59 +0200

    Initial Commit

---
+++ Tedee Recruitment Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tedee Recruitment Task
+//
+//  Created by Tomasz Paluch on 30/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
